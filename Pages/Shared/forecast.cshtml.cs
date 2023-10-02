449dfda57d8f8e447fae298bae2999e73535b634
ZhangXin <zhang-xin@hotmail.com>
2023-10-02 19:47:17 +0300

    添加项目文件。

---
+++ Pages/Shared/forecast.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Forecast_Team8.Pages
+{
+    public class forecastModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public forecastModel(ILogger<forecastModel> logger)
+        {
+            _logger = (ILogger<IndexModel>?)logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
